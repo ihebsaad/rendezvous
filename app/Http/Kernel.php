0ed4566d767b0fc4f8a4f9ff0c10c9e18bbb8e49
iheb <ihebsaad@gmail.com>
2020-11-02 12:04:02 +0100

    Https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,12 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+		
+		
+    // appending custom middleware 
+	\App\Http\Middleware\HttpsProtocol::class,
+	
+	
     ];
 
     /**
