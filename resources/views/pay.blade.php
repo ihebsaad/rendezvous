f1c3cbb61a1d35c746ae4cf5c28b5cdd26463335
iheb <ihebsaad@gmail.com>
2020-11-24 15:42:22 +0100

    style et messages

--- resources/views/pay.blade.php
+++ resources/views/pay.blade.php
@@ -3,13 +3,32 @@
  @section('content')
  <div class="container">
  <div style="max-width:600px"> 
+ 
+        @if ($errors->any())
+             <div class="alert alert-danger">
+                 <ul>
+                     @foreach ($errors->all() as $error)
+                         <li>{{ $error }}</li>
+                     @endforeach
+                 </ul>
+             </div><br />
+         @endif
+
+    @if (!empty( Session::get('success') ))
+        <div class="alert alert-success">
+
+        {{ Session::get('success') }}
+        </div>
+
+    @endif
+	
 <form class="  " method="POST" id="payment-form"    action="{{ route('paypal') }}" >
   {{ csrf_field() }}
   <h2 class="w3-text-blue">Paiement</h2>
    <p>      
-  <label class="w3-text-blue"><b>Montant</b></label>
-  <input class="w3-input w3-border" name="amount" type="text"></p>      
-  <button class="w3-btn w3-blue">Payer</button></p>
+  <label class=" "><b>Montant</b></label>
+  <input class="form-control " name="amount" type="text"></p>      
+  <button class="button ">Payer</button></p>
 </form>
 
 </div>
