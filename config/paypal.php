b59d916e518346f6968227bfb95d55f294245056
iheb <ihebsaad@gmail.com>
2020-11-24 14:15:20 +0100

    test

--- config/paypal.php
+++ config/paypal.php
@@ -1,9 +1,9 @@
 <?php 
 return [ 
-    'client_id' => env('PAYPAL_CLIENT_ID',''),
-    'secret' => env('PAYPAL_SECRET',''),
+    'client_id' => 'AalCYOUCLECAZi8JkWpRCQB6FDP1acLmsSWC0dKnF3R6dVORwWfo2YExwJFdwZI3qd8mS8ZIXvrvzYy6' //env('PAYPAL_CLIENT_ID',''),
+    'secret' => 'ENHh0jbRemqhawfb7LvSjAfV3bPuJ4s1f0dzdvI37FvjIf-tUnOFatwMwSsNw50k7rbGuThAgSsQ-aSi' //env('PAYPAL_SECRET',''),
     'settings' => array(
-        'mode' => env('PAYPAL_MODE','sandbox'),
+        'mode' => 'sandbox' // env('PAYPAL_MODE','sandbox'),
         'http.ConnectionTimeOut' => 30,
         'log.LogEnabled' => true,
         'log.FileName' => storage_path() . '/logs/paypal.log',
