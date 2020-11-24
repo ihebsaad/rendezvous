41535d0c4ae1be4454b8948440ddc4663fcbfcde
iheb <ihebsaad@gmail.com>
2020-11-24 15:34:37 +0100

    mise à jour style

--- resources/views/pay.blade.php
+++ resources/views/pay.blade.php
@@ -1,12 +1,18 @@
  @extends('layouts.frontlayout')
  
  @section('content')
+ <div class="container">
+ <div style="max-width:600px"> 
 <form class="  " method="POST" id="payment-form"    action="{{ route('paypal') }}" >
   {{ csrf_field() }}
-  <h2 class="w3-text-blue">Payment Form</h2>
+  <h2 class="w3-text-blue">Paiement</h2>
    <p>      
   <label class="w3-text-blue"><b>Montant</b></label>
   <input class="w3-input w3-border" name="amount" type="text"></p>      
   <button class="w3-btn w3-blue">Payer</button></p>
 </form>
+
+</div>
+
+</div>
  @endsection
