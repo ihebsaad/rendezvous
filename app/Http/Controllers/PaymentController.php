7c6723c891c2d450e43adc85eac7fe165d7c688e
iheb <ihebsaad@gmail.com>
2020-12-07 17:07:11 +0100

    test

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -461,7 +461,7 @@ class PaymentController extends Controller
 		  if($expiration> $today){
 			 // expiration + abonnement
 
-			 $datee=$prestataire->expire->addDays(31);
+			// $datee=$prestataire->expire->addDays(31);
 			 $prestataire->expire->addDays(31);
 		     $prestataire->save();
 		 }
