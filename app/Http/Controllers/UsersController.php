4259862d9e8cf9ea582ff0840d6f6fb187b95d32
haythemsahlia <haithemsahlia@gmail.com>
2020-12-16 15:42:58 +0100

    store test

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -264,14 +264,19 @@ class UsersController extends Controller
 	{
  	 //$temp_file = $_FILES['file']['tmp_name'];
 
-		 $name='';
+		 /*$name='';
 		if($request->file('file')!=null)
 		{$image=$request->file('file');
 		 $name =  $image->getClientOriginalName();
                  $path = storage_path()."/images/";
  
           //$image->move($path, $name);
-                 $image->store($path, $name);
+		}*/
+		$image = $request->file('file');
+		$destinationPathImg = storage_path()."/images/";
+
+		if (!$image->move($destinationPathImg, $image->getClientOriginalName())) {
+		    return 'Error saving the file.';
 		}
 		  DB::table('parametres')->where('id', 1)->update(array('video' => $name));
   
