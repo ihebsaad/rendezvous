0adb18d3083387aa121a586548c592b042ac5497
haythemsahlia <haithemsahlia@gmail.com>
2020-12-28 11:56:17 +0100

    test msg

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -405,7 +405,7 @@ class UsersController extends Controller
 		    '+21654076876',
 		    array(
 		        'from' => $twilio_number,
-		        'body' => 'test de prendez un rendez vous!'
+		        'body' => '/n Bonjour, /n test du site: prenezunrendezvous.com !'
 		    )
 		);
 		// creation service
