71510d1cadfefee7cab90c84aeb8080a425a9e38
haythemsahlia <haithemsahlia@gmail.com>
2020-12-28 11:58:04 +0100

    fix

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -405,7 +405,7 @@ class UsersController extends Controller
 		    '+21654076876',
 		    array(
 		        'from' => $twilio_number,
-		        'body' => '/n Bonjour, /n test du site: prenezunrendezvous.com !'
+		        'body' => '%0a%0a Bonjour, %0a test du site: prenezunrendezvous.com !'
 		    )
 		);
 		// creation service
