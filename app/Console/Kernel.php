a93c8d3ec52f1329cdffec87501716fdb0c31815
haythemsahlia <haithemsahlia@gmail.com>
2020-12-29 15:33:46 +0100

    add log

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('reminders:send')->everyMinute();
+        $schedule->command('reminders:send')->everyMinute()-> appendOutputTo ('/my/logs/laravel_output.log');
     }
 
     /**
