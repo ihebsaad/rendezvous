972105554bf3125138fcac719962f575265a8428
haythemsahlia <haithemsahlia@gmail.com>
2021-01-13 11:30:49 +0100

    abonnement front

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -6,6 +6,68 @@
  $parametres=DB::table('parametres')->where('id', 1)->first();
 
  ?>
+ <style type="text/css">
+ /* highlight PLAN 2*/
+.brilliant::before {
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 64px 64px 0 0;
+  border-color: #006ed2 transparent transparent transparent;
+  position: absolute;
+  left: 15px;
+  top: 0;
+  content: "";
+}
+.brilliant::after {
+  color: white;
+  position: absolute;
+  left: 24px;
+  top: 6px;
+  text-shadow: 0 0 2px #006ed2;
+  font-size: 2.2rem;
+}
+.brilliant::after{
+  font-family: "FontAwesome";
+  content: "";
+}
+/* show more plan 2 & 3*/
+.read-more-state {
+  display: none!important;
+}
+
+.read-more-target {
+  opacity: 0;
+  max-height: 0;
+  padding: 0px 0!important;
+  font-size: 0;
+  transition: .25s ease;
+}
+
+.read-more-state:checked ~ .read-more-wrap .read-more-target {
+  opacity: 1;
+  font-size: inherit;
+  max-height: 999em;
+  padding: 6px 0!important;
+}
+
+.read-more-state ~ .read-more-trigger:before {
+  content: 'Plus';
+}
+
+.read-more-state:checked ~ .read-more-trigger:before {
+  content: 'Moins';
+}
+
+.read-more-trigger {
+  cursor: pointer;
+  display: inline-block;
+  text-transform: uppercase;
+  font-weight: 700;
+  color: #006ed2;
+  line-height: 2;
+}
+ </style>
 <br>
   <section class="fullwidth_block margin-top-0 padding-top-0 padding-bottom-50" data-background-color="#fff"> 
     <div class="container">
@@ -25,14 +87,28 @@
         </div>
         
               <div class="utf_price_plan_features">
-                <ul>
-                  <li>Entreprise publiée pour <b style="color:green"><u>30 jours</u></b></li>
-                  <li>Réservations des services</li>  
-                  <li>Rappels SMS</li>          
-                  <li><b style="color:green">Entreprise Mise en Avant</b></li>
-                  <li><i class="sl sl-icon-check"></i> <b style="color:green">Commission de <u> <?php echo $parametres->commission_abonnement3;?></u>%</b> sur les services réservés</li>
+              	<input type="checkbox" class="read-more-state" id="post-3" />
+                <ul class="read-more-wrap">
+                  <li>Réservations en ligne illimitées <b style="color:green">24/7</b></li>
+                  <li>Rappels SMS illimités</li>
+                  <li><b style="color:green">Entreprise mise en avant</b> dans la recherche</li>
+                  <li>Rappels SMS</li>		
+                  <li><b style="color:green">Acompte de 60%</b> lors de la prise de RDV pour les services</li>	
+                  <li>Codes promo</li> 
+                  <li class="read-more-target">Carte de fidélité virtuelle <b style="color:green">(9 séances achetées et la 10ème prestation aura une réduction)</b></li> 
+                  <li class="read-more-target">Paiement en plusieurs fois <b style="color:green">à partir de 200€</b></li>
+                  <li class="read-more-target">Tableau de statistiques des ventes, des services et des produits</li>
+                  <li class="read-more-target">Service client accessible <b style="color:green">7J/7</b></li>
+                  <li class="read-more-target">Choisir les jours et les heures d'indisponibilité dans votre calendrier</li>
+                  <li class="read-more-target">Obtenir un QR code qui va ramener à votre page</li>
+                  <li class="read-more-target">Système de gestion des heures creuses "happy hours"</li>
+                  <li class="read-more-target">Synchronisation du calendrier du site avec votre google agenda</li>
+                  <li class="read-more-target">Agenda flexible</li>
+                  <li class="read-more-target">Service récurrent</li>                  
+                  <li class="read-more-target">Boutique de vente de produits physiques et numériques</li>
                 </ul>
-              @guest  <a class="button sign-in popup-with-zoom-anim" href="#dialog_signin_part" onclick="$('#litab2').trigger('click');"><i class="sl sl-icon-basket"></i> Acheter</a> 
+                 <label for="post-3" class="read-more-trigger"></label></br>
+              @guest  <a class="button border sign-in popup-with-zoom-anim" href="#dialog_signin_part" onclick="$('#litab2').trigger('click');"><i class="sl sl-icon-basket"></i> Acheter</a> 
        @else
         <form class="  " method="POST" id="payment-form"    action="{{ route('payabn') }}" >
         {{ csrf_field() }}
@@ -49,20 +125,27 @@
 
       
       <!-- plan 2 - start -->        
-      <div class="plan featured col-md-4 col-sm-6 col-xs-12 active">
-        <div class="utf_price_plan">
+      <div class="plan featured brilliant col-md-4 col-sm-6 col-xs-12 active">
+        <div class="utf_price_plan" style="background-color: #3d92e0;">
           <h3><?php echo $parametres->abonnement2;?></h3>
           <span class="value"><?php echo $parametres->cout_abonnement2;?>€<span>/Par Mois</span></span> <span class="period">Abonnement <?php echo $parametres->abonnement2;?></span> 
-			  </div>
+		</div>
               <div class="utf_price_plan_features">
-                <ul>
-                  <li>Entreprise publiée pour <u>30 jours</u></li>
-                  <li>Réservations des services</li>
-                  <li>Rappels SMS</li>				  
-                  <li><b style="color:green">Entreprise Mise en Avant</b></li>
-                  <li>Commission de <b style="color:green"><u><?php echo $parametres->commission_abonnement2;?></u>%</b> sur les services réservés</li>
+              	<input type="checkbox" class="read-more-state" id="post-2" />
+                <ul class="read-more-wrap">
+                  <li>Réservations en ligne illimitées <b style="color:green">24/7</b></li>
+                  <li>Rappels SMS illimités</li>
+                  <li><b style="color:green">Entreprise mise en avant</b> dans la recherche</li>
+                  <li>Rappels SMS</li>		
+                  <li><b style="color:green">Acompte de 50%</b> lors de la prise de RDV pour les services</li>	 
+                  <li>Codes promo</li>
+                  <li class="read-more-target">Carte de fidélité virtuelle <b style="color:green">(9 séances achetées et la 10ème prestation aura une réduction)</b></li> 
+                  <li class="read-more-target">Paiement en plusieurs fois <b style="color:green">à partir de 200€</b></li>
+                  <li class="read-more-target">Tableau de statistiques des ventes et des services</li>
+                  <li class="read-more-target">Service client accessible <b style="color:green">6J/7</b></li>
                  </ul>
-                @guest <a class="button border sign-in popup-with-zoom-anim" href="#dialog_signin_part" onclick="$('#litab2').trigger('click');"><i class="sl sl-icon-basket"></i> Acheter</a> 
+                 <label for="post-2" class="read-more-trigger"></label></br>
+                @guest <a class="button sign-in popup-with-zoom-anim" href="#dialog_signin_part" onclick="$('#litab2').trigger('click');" ><i class="sl sl-icon-basket"></i> Acheter</a> 
 			 @else
 			 <form class="  " method="POST" id="payment-form"    action="{{ route('payabn') }}" >
 				{{ csrf_field() }}
@@ -84,11 +167,13 @@
         </div>
         
               <div class="utf_price_plan_features">
-                <ul>
-                  <li>Entreprise publiée pour <u>30 jours</u></li>
-                  <li>Réservations des services</li>
-                  <li>Rappels SMS</li>
-                  <li>Commission de <u><?php echo $parametres->commission_abonnement1;?>%</u> sur les services réservés</li>
+                <ul style="padding-bottom: 28px">
+                  <li>Réservations en ligne illimitées <b style="color:green">24/7</b></li>
+                  <li>Rappels SMS illimités</li>
+                  <li><b style="color:green">Acompte de 30%</b> lors de la prise de RDV pour les services</li>	
+                  <li>Carte de fidélité virtuelle <b style="color:green">(5 séances achetées et la 6ème prestation aura une réduction)</b></li>
+                  <li>Service client accessible <b style="color:green">6J/7</b></li>
+                  <?php // echo $parametres->commission_abonnement1;?>
                  </ul>
                  <br>
                @guest <a class="button border sign-in popup-with-zoom-anim" href="#dialog_signin_part" onclick="$('#litab2').trigger('click');"><i class="sl sl-icon-basket"></i> Acheter</a> 
