b4f51d0d7606689f87e02062d73913396c00902b
kbskhaled <kbskhaled@gmail.com>
2021-01-26 17:01:13 +0100

    correction script

--- resources/views/viewlisting.blade.php
+++ resources/views/viewlisting.blade.php
@@ -1082,8 +1082,7 @@ $("body").mouseup(function() {
 	if (!mouse_is_inside) close_panel_dropdown();
 });
 </script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script src="{{  URL::asset('public/css/slider_carre/carousel.js') }}" type="text/javascript"></script> 
 <script>
