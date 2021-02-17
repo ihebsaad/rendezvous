0ad9f04452a178e6b0e0462a3ae35f3cae7611c4
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-02-17 17:41:56 +0100

    auth redirect to home

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,6 @@ Route::get('/dashboard', function () {
 });
 */
 Route::get('/', array('as' => 'home','uses' => 'UsersController@home'));
-Route::get('/dashboard', array('as' => 'dashboard','uses' => 'UsersController@dashboard'));
-Route::get('/listings', array('as' => 'listings','uses' => 'UsersController@listings'));
-Route::get('/users', array('as' => 'users','uses' => 'UsersController@index'));
 Route::get('/prestataires', 'UsersController@prestataires')->name('prestataires');
 Route::get('/pricing', 'UsersController@pricing')->name('pricing');
 Route::get('/abonnements', 'UsersController@abonnements')->name('abonnements');
@@ -40,91 +37,100 @@ Route::get('/apropos', 'UsersController@apropos')->name('apropos');
 Route::get('/faqs', 'UsersController@faqs')->name('faqs');
 Route::get('/conditions-utilisation', 'UsersController@ConditionsUtilisation')->name('ConditionsUtilisation');
 
-Route::get('/profile/{id}', 'UsersController@profile')->name('profile');
-Route::get('/listing/{id}', 'UsersController@listing')->name('listing');
-//Route::get('/view/{id}', 'UsersController@viewlisting')->name('viewlisting');
-Route::get('/{slug}/{id}', 'UsersController@viewlisting');
-Route::post('/users/updating','UsersController@updating')->name('users.updating');
-Route::post('/users/ajoutimage','UsersController@ajoutimage')->name('users.ajoutimage');
-Route::post('/users/ajoutvideo','UsersController@ajoutvideo')->name('users.ajoutvideo');
-Route::post('/users/ajoutimages','UsersController@ajoutimages')->name('users.ajoutimages');
-Route::post('/users/ajoutcouv','UsersController@ajoutcouv')->name('users.ajoutcouv');
-Route::get('/users/removeimage/{id}/{user}', 'UsersController@removeimage');
-Route::get('/users/removevideo/{id}', 'UsersController@removevideo');
-Route::get('/users/remove/{id}', 'UsersController@remove');
-Route::get('/users/destroy/{id}', 'UsersController@destroy');
-
-Route::post('/users/parametring','UsersController@parametring')->name('users.parametring');
-Route::get('/parametres','UsersController@parametres')->name('parametres');
-Route::post('/users/ajoutlogo','UsersController@ajoutlogo')->name('users.ajoutlogo');
-Route::post('/users/ajoutvideoslider','UsersController@ajoutvideoslider')->name('users.ajoutvideoslider');
-Route::post('/users/sendsms','UsersController@sendsms')->name('users.sendsms');
-Route::post('/services/saving','ServicesController@store')->name('services.saving');
-Route::post('/services/add','ServicesController@add')->name('services.add');
-Route::post('/services/store','ServicesController@store')->name('services.store');
-Route::get('/services/add','ServicesController@add')->name('services.add');
-Route::post('/services/updating','ServicesController@updating')->name('services.updating'); 
-Route::get('/services/remove/{id}/{user}', 'ServicesController@remove');
 
 
+Route::group(['middleware' => 'auth'], function(){
 
-Route::post('/faqs/saving','FaqsController@store')->name('faqs.saving');
-Route::post('/faqs/add','FaqsController@add')->name('faqs.add');
-Route::get('/faqs/add','FaqsController@add')->name('faqs.add');
-Route::post('/faqs/updating','FaqsController@updating')->name('faqs.updating'); 
-Route::get('/faqs/remove/{id}/{user}', 'FaqsController@remove');
+	Route::get('/dashboard', array('as' => 'dashboard','uses' => 'UsersController@dashboard'));
+	Route::get('/listings', array('as' => 'listings','uses' => 'UsersController@listings'));
+	Route::get('/users', array('as' => 'users','uses' => 'UsersController@index'));
 
-Route::get('/reviews', array('as' => 'reviews','uses' => 'ReviewsController@index'));
-Route::post('/reviews/add','ReviewsController@add')->name('reviews.add');
-Route::post('/reviews/remove/{id}','ReviewsController@remove')->name('reviews.remove');
-Route::post('/reviews/addfavoris','ReviewsController@addfavoris')->name('reviews.addfavoris');
-Route::post('/reviews/removefavoris','ReviewsController@removefavoris')->name('reviews.removefavoris');
-Route::post('/reviews/removefavoris','ReviewsController@removefavoris')->name('reviews.removefavoris');
-Route::get('/favoris','UsersController@favoris')->name('favoris');
+	Route::get('/profile/{id}', 'UsersController@profile')->name('profile');
+	Route::get('/listing/{id}', 'UsersController@listing')->name('listing');
+	//Route::get('/view/{id}', 'UsersController@viewlisting')->name('viewlisting');
+	Route::get('/{slug}/{id}', 'UsersController@viewlisting');
+	Route::post('/users/updating','UsersController@updating')->name('users.updating');
+	Route::post('/users/ajoutimage','UsersController@ajoutimage')->name('users.ajoutimage');
+	Route::post('/users/ajoutvideo','UsersController@ajoutvideo')->name('users.ajoutvideo');
+	Route::post('/users/ajoutimages','UsersController@ajoutimages')->name('users.ajoutimages');
+	Route::post('/users/ajoutcouv','UsersController@ajoutcouv')->name('users.ajoutcouv');
+	Route::get('/users/removeimage/{id}/{user}', 'UsersController@removeimage');
+	Route::get('/users/removevideo/{id}', 'UsersController@removevideo');
+	Route::get('/users/remove/{id}', 'UsersController@remove');
+	Route::get('/users/destroy/{id}', 'UsersController@destroy');
 
-
-Route::post('/reservations/add','ReservationsController@add')->name('reservations.add');
-Route::get('/reservations','ReservationsController@index')->name('reservations');
-Route::get('/reservations/remove/{id}','ReservationsController@remove');
-Route::get('/reservations/valider/{id}','ReservationsController@valider');
-Route::get('/reservations/annuler/{id}','ReservationsController@annuler');
-Route::post('/reservations/sendmessage','ReservationsController@sendmessage')->name('reservations.sendmessage');
-Route::post('/reservations/contactmessage','ReservationsController@contactmessage')->name('reservations.contactmessage');
-
-
-Route::get('/categories', array('as' => 'categories','uses' => 'CategoriesController@index'));
-Route::post('/categories/insert','CategoriesController@insert')->name('categories.insert');
-Route::post('/categories/add','CategoriesController@add')->name('categories.add');
-Route::get('/categories/add','CategoriesController@add')->name('categories.add');
-Route::post('/categories/updating','CategoriesController@updating')->name('categories.updating'); 
-Route::get('/categories/remove/{id}', 'CategoriesController@remove');
-Route::post('/categories/removecatuser', 'CategoriesController@removecatuser')->name('categories.removecatuser'); 
-
-Route::post('/payabn','PaymentController@payabn')->name('payabn');
-Route::get('/payabn','PaymentController@payabn')->name('payabn');
-Route::get('/statusabn','PaymentController@getPaymentStatusAbn')->name('statusabn');
-
-
-Route::post('/pay','PaymentController@pay')->name('pay');
-Route::get('/pay','PaymentController@pay')->name('pay');
-Route::post('/paypal','PaymentController@payWithpaypal')->name('paypal');
-Route::post('/payreservation','PaymentController@payreservation')->name('payreservation');
-Route::get('/payreservation','PaymentController@payreservation')->name('payreservation');
-Route::get('/status','PaymentController@getPaymentStatus')->name('status');
-Route::get('/statusres','PaymentController@getPaymentStatusRes')->name('statusres');
-Route::post('/statusres','PaymentController@getPaymentStatusRes')->name('statusres');
-
-
-Route::get('/abonnements','AbonnementsController@index')->name('abonnements');
-Route::get('/abonnements/remove/{id}', 'AbonnementsController@remove');
+	Route::post('/users/parametring','UsersController@parametring')->name('users.parametring');
+	Route::get('/parametres','UsersController@parametres')->name('parametres');
+	Route::post('/users/ajoutlogo','UsersController@ajoutlogo')->name('users.ajoutlogo');
+	Route::post('/users/ajoutvideoslider','UsersController@ajoutvideoslider')->name('users.ajoutvideoslider');
+	Route::post('/users/sendsms','UsersController@sendsms')->name('users.sendsms');
+	Route::post('/services/saving','ServicesController@store')->name('services.saving');
+	Route::post('/services/add','ServicesController@add')->name('services.add');
+	Route::post('/services/store','ServicesController@store')->name('services.store');
+	Route::get('/services/add','ServicesController@add')->name('services.add');
+	Route::post('/services/updating','ServicesController@updating')->name('services.updating'); 
+	Route::get('/services/remove/{id}/{user}', 'ServicesController@remove');
 
 
 
-Route::get('/alertes','AlertesController@index')->name('alertes');
-Route::get('/alertes/remove/{id}', 'AlertesController@remove');
+	Route::post('/faqs/saving','FaqsController@store')->name('faqs.saving');
+	Route::post('/faqs/add','FaqsController@add')->name('faqs.add');
+	Route::get('/faqs/add','FaqsController@add')->name('faqs.add');
+	Route::post('/faqs/updating','FaqsController@updating')->name('faqs.updating'); 
+	Route::get('/faqs/remove/{id}/{user}', 'FaqsController@remove');
+
+	Route::get('/reviews', array('as' => 'reviews','uses' => 'ReviewsController@index'));
+	Route::post('/reviews/add','ReviewsController@add')->name('reviews.add');
+	Route::post('/reviews/remove/{id}','ReviewsController@remove')->name('reviews.remove');
+	Route::post('/reviews/addfavoris','ReviewsController@addfavoris')->name('reviews.addfavoris');
+	Route::post('/reviews/removefavoris','ReviewsController@removefavoris')->name('reviews.removefavoris');
+	Route::post('/reviews/removefavoris','ReviewsController@removefavoris')->name('reviews.removefavoris');
+	Route::get('/favoris','UsersController@favoris')->name('favoris');
 
 
-Route::get('/payments','PaymentController@index')->name('payments');
-Route::get('/payments/remove/{id}', 'PaymentController@remove');
+	Route::post('/reservations/add','ReservationsController@add')->name('reservations.add');
+	Route::get('/reservations','ReservationsController@index')->name('reservations');
+	Route::get('/reservations/remove/{id}','ReservationsController@remove');
+	Route::get('/reservations/valider/{id}','ReservationsController@valider');
+	Route::get('/reservations/annuler/{id}','ReservationsController@annuler');
+	Route::post('/reservations/sendmessage','ReservationsController@sendmessage')->name('reservations.sendmessage');
+	Route::post('/reservations/contactmessage','ReservationsController@contactmessage')->name('reservations.contactmessage');
 
-Route::post('/search_prestataires','RechercheController@search_prestataires')->name('search.prestataires');
+
+	Route::get('/categories', array('as' => 'categories','uses' => 'CategoriesController@index'));
+	Route::post('/categories/insert','CategoriesController@insert')->name('categories.insert');
+	Route::post('/categories/add','CategoriesController@add')->name('categories.add');
+	Route::get('/categories/add','CategoriesController@add')->name('categories.add');
+	Route::post('/categories/updating','CategoriesController@updating')->name('categories.updating'); 
+	Route::get('/categories/remove/{id}', 'CategoriesController@remove');
+	Route::post('/categories/removecatuser', 'CategoriesController@removecatuser')->name('categories.removecatuser'); 
+
+	Route::post('/payabn','PaymentController@payabn')->name('payabn');
+	Route::get('/payabn','PaymentController@payabn')->name('payabn');
+	Route::get('/statusabn','PaymentController@getPaymentStatusAbn')->name('statusabn');
+
+
+	Route::post('/pay','PaymentController@pay')->name('pay');
+	Route::get('/pay','PaymentController@pay')->name('pay');
+	Route::post('/paypal','PaymentController@payWithpaypal')->name('paypal');
+	Route::get('/payreservation','PaymentController@payreservation')->name('payreservation');
+	Route::get('/status','PaymentController@getPaymentStatus')->name('status');
+	Route::get('/statusres','PaymentController@getPaymentStatusRes')->name('statusres');
+	Route::post('/statusres','PaymentController@getPaymentStatusRes')->name('statusres');
+
+
+	Route::get('/abonnements','AbonnementsController@index')->name('abonnements');
+	Route::get('/abonnements/remove/{id}', 'AbonnementsController@remove');
+
+
+
+	Route::get('/alertes','AlertesController@index')->name('alertes');
+	Route::get('/alertes/remove/{id}', 'AlertesController@remove');
+
+
+	Route::get('/payments','PaymentController@index')->name('payments');
+	Route::get('/payments/remove/{id}', 'PaymentController@remove');
+
+	Route::post('/search_prestataires','RechercheController@search_prestataires')->name('search.prestataires');
+});
+
