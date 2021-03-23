21ec83ad556390fabea1a5975a10f03c4a5687a3
kbskhaled <kbskhaled@gmail.com>
2021-03-23 10:19:20 +0100

    test

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -52,6 +52,10 @@ class ReservationsController extends Controller
 		if($cuser->user_type=='admin' ){
         $reservations = Reservation::orderBy('id','asc')->get();
 		}	
+		 $reservations=$reservations->sortBy(function($t)
+                                        {
+                                            return $t->id;
+                                        })->reverse();
         dd( $reservations);
 		//$this->sendMail('ihebsaad@gmail.com','Test','test Hello world')	;
         return view('reservations.index', compact('reservations'));
