d37fb9a5475e7e3260262e03e406cc774afa2ba3
iheb <ihebsaad@gmail.com>
2021-04-19 14:22:14 +0100

    test

--- config/paypal.php
+++ config/paypal.php
@@ -5,8 +5,8 @@ return [
     'secret' => // test 'ENHh0jbRemqhawfb7LvSjAfV3bPuJ4s1f0dzdvI37FvjIf-tUnOFatwMwSsNw50k7rbGuThAgSsQ-aSi', //env('PAYPAL_SECRET',''),
     'EKIQ0nFX0aqJ4L4kodsY1FMAr6g3PH3TNc9fJ8IGyvcBoFAOw5XL1CPpk886lYQtnivgbaVWTC-WWb2-',
     'settings' => array(
-        //'mode' => 'sandbox', 
-        'mode' => 'live',
+         'mode' => 'sandbox', 
+       // 'mode' => 'live',
         'http.ConnectionTimeOut' => 30,
         'log.LogEnabled' => true,
         'log.FileName' => storage_path() . '/logs/paypal.log',
