720a590f1a86e9bb8e1b159d290c15fa7bb198ac
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-04-20 11:31:23 +0200

    test

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'ihebs009@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'contact@prenezunrendezvous.com'),
         'name' => env('MAIL_FROM_NAME', 'Prenez Un Rendez Vous'),
     ],
 
