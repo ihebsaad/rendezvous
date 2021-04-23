b44a523292f4633b04882a1424a4e872afeb9f0f
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-04-23 10:41:01 +0200

    test paypal api

--- app/Http/Controllers/MyPaypalController.php
+++ app/Http/Controllers/MyPaypalController.php
@@ -1,11 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use Route;
+use App\Invoice;
+use App\IPNStatus;
+use App\Item;
+use App\User;
+use Session;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Srmklive\PayPal\Services\AdaptivePayments;
+use Srmklive\PayPal\Services\ExpressCheckout;
 
 class MyPaypalController extends Controller
 {
+	protected $provider;
     //-------------------------------------------payAcompteReservation---------------------------------------------
     public function payAcompteReservation(Request $request)
     {
