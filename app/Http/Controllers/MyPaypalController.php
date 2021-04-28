627af086da3b010065fdb0d83a1b95936c48f1d8
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-04-28 22:32:58 +0200

    test

--- app/Http/Controllers/MyPaypalController.php
+++ app/Http/Controllers/MyPaypalController.php
@@ -87,7 +87,7 @@ class MyPaypalController extends Controller
         ];
 
         $response = $this->provider->createPayRequest($data);
-        //dd($response);
+        dd($response);
 //$redirect_url = $this->provider->getRedirectUrl('approved', $response['payKey']);
 
 return $response;
