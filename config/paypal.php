7c8491a80f5ef45958a34132c00ebd612621f03e
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-05-24 12:49:34 +0200

    test

--- config/paypal.php
+++ config/paypal.php
@@ -2,10 +2,10 @@
 return [ 
     'client_id' => // test 'AalCYOUCLECAZi8JkWpRCQB6FDP1acLmsSWC0dKnF3R6dVORwWfo2YExwJFdwZI3qd8mS8ZIXvrvzYy6', //env('PAYPAL_CLIENT_ID',''),
    // 'AQhBOI4hV00Ad1KVimAgdA6uelgGBuRMAGmOhfJ9ETDZKsDATzwwKKnCjjnSCAfqb7fjfEFk4cisbp6_',
-    'AalCYOUCLECAZi8JkWpRCQB6FDP1acLmsSWC0dKnF3R6dVORwWfo2YExwJFdwZI3qd8mS8ZIXvrvzYy6',
+    'AThjz56X69vxz_voqkSvfOaCPslWkDa6mwdZF4Pe7jqKG_qN9iA8L09Kapl1fpUVVfpnjM5b06qttYY-',
     'secret' => // test 'ENHh0jbRemqhawfb7LvSjAfV3bPuJ4s1f0dzdvI37FvjIf-tUnOFatwMwSsNw50k7rbGuThAgSsQ-aSi', //env('PAYPAL_SECRET',''),
   //  'EKIQ0nFX0aqJ4L4kodsY1FMAr6g3PH3TNc9fJ8IGyvcBoFAOw5XL1CPpk886lYQtnivgbaVWTC-WWb2-',
-    'ENHh0jbRemqhawfb7LvSjAfV3bPuJ4s1f0dzdvI37FvjIf-tUnOFatwMwSsNw50k7rbGuThAgSsQ-aSi',
+    'EM10ylTG8nI5JGyMQKkADrS_ZRg0lTYNdJVPJrTuPLrb_91X0R4cASKrYZSnwLKQ-tGrEuO3A5vqVGfx',
     'settings' => array(
          'mode' => 'sandbox', 
        // 'mode' => 'live',
