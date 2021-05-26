06c1eee55036412933b823e05fffea6ef6a0ec6d
kbskhaled <kbskhaled@gmail.com>
2021-05-26 16:41:20 +0100

    test

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -238,7 +238,7 @@ $allow_slices = UsersController::ChampById('allow_slices',$reservation->prestata
                         $servicesres = json_encode($reservation->services_reserves);
                       }
 
-                     $servicesres=(Array)$servicesres;
+                     $servicesres=(array)$servicesres;
                     dd($servicesres);
                       foreach ($servicesres as $servicesre) {
                       // dd($servicesre);
