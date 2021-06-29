faf17ba83bb074b82b447ddcd0798f9a222a6c99
kbskhaled <kbskhaled@gmail.com>
2021-06-29 15:38:49 +0100

    test

--- resources/views/users/listing.blade.php
+++ resources/views/users/listing.blade.php
@@ -2320,7 +2320,7 @@ if (x=="Numérique") {
          {val=1;}else{val=0;} 
 	 }
                 var user = $('#user').val();
-                alert("user="+user);
+               // alert("user="+user);
                 //if ( (val != '')) {
                 var _token = $('input[name="_token"]').val();
                 $.ajax({
