020f4d3154de48a485adf08bf08a633c86257b19
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-08-09 22:55:00 +0200

    test

--- resources/views/payments/payAbn2.blade.php
+++ resources/views/payments/payAbn2.blade.php
@@ -56,7 +56,7 @@ background-color:#a0d468;
             <div class="col-md-8 col-md-offset-2">
            
               <div class="dashboard-list-box with-icons margin-top-20">
-                  <h4><i class="fa fa-address-book" aria-hidden="true"></i>Paiement de votre abonnement </h4>      
+                  <h4><i class="sl sl-icon-location"></i>Paiement de votre abonnement </h4>      
                 <ul>
                                <li> <form action="#" class="my-4">
                                     <div id="card-element">
