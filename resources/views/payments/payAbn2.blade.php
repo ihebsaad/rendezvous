9004abc05038cfee16bb4b69d255c3265cafa1ab
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-08-09 22:21:52 +0200

    test

--- resources/views/payments/payAbn2.blade.php
+++ resources/views/payments/payAbn2.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.frontv2layout')
  
 <script src="https://js.stripe.com/v3/"></script>
-
+ <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css" />
  <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/dataTables.bootstrap.css') }}" />
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/buttons.bootstrap.css') }}" />
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/assets/datatables/css/scroller.bootstrap.css') }}" />
@@ -73,6 +73,9 @@ background-color:#a0d468;
 
             <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script src="https://momentjs.com/downloads/moment.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
 <script>
   $('.btn').on('click', function() {
     var $this = $(this);
