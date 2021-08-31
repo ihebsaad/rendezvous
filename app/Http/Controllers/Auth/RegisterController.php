7a4ead6fa7f1b5293a0f9012ded70f441f03dcfe
kbskhaled <kbskhaled@gmail.com>
2021-08-31 15:39:45 +0100

    test abonn classique

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -238,7 +238,7 @@ class RegisterController extends Controller
           
             $nbprest=User::where('user_type','prestataire')->whereNotNull('expire')->count();
 
-            if( $nbprest > 100)
+            if( $nbprest < 100)
             {
              return redirect ('/pricing');
             }
