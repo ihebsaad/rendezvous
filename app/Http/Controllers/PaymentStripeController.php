59c13deec845ca6eb924eda63c5cecf57a7e0a1c
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-09-01 17:23:02 +0200

    test

--- app/Http/Controllers/PaymentStripeController.php
+++ app/Http/Controllers/PaymentStripeController.php
@@ -1000,63 +1000,37 @@ public function sendMail($to,$sujet,$contenu){
 
 public function stripeWebhook(Request $request)
     {
-      dd("ok");
       \Stripe\Stripe::setApiKey('sk_test_51IyZEOLYsTAPmLSFOUPFtTTEusJc2G7LSMDZEYDxBsv0iJblsOpt1dfaYu8PrEE6iX6IX7rCbpifzhdPfW7S0lzA007Y8kjGAx');
-        // You can find your endpoint's secret in your webhook settings
-        $endpoint_secret = "whsec_kJ1wYb3HxPXQ5SmCvdmGsoEngJCxJ0bg";
-
-        $payload = @file_get_contents('php://input');
-        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
-        $event = null;
-
-        try
-        {
-            $event = \Stripe\Webhook::constructEvent($payload, $sig_header, $endpoint_secret);
-        } 
-        catch(\UnexpectedValueException $e)
-        {
-                // Invalid payload
-                return response()->json([
-                    'message' => 'Invalid payload',
-                ], 200);
-        }
-        catch(\Stripe\Exception\SignatureVerificationException $e)
-        {
-            // Invalid signature
-            return response()->json([
-                'message' => 'Invalid signature',
-            ], 200);
-        }
-
-        if ($event->type == "payment_intent.succeeded")
-        {
-            //As I understand here is where I should do things like send order info by mail and deplete stock accordingly
-
-            $intent = $event->data->object;
-
-            //$this->completeOrderInDatabase()
-            //$this->sendMail();
-
-            return response()->json([
-                'intentId' => $intent->id,
-                'message' => 'Payment succeded'
-            ], 200); 
-        } 
-        elseif ($event->type == "payment_intent.payment_failed")
-        {
-            //Payment failed to be completed
-            
-            $intent = $event->data->object;
-            $error_message = $intent->last_payment_error ? $intent->last_payment_error->message : "";
-
-            return response()->json([
-                'intentId' => $intent->id,
-                'message' => 'Payment failed: '.$error_message
-            ], 400); 
-        }
-
-        
-    }
+$payload = @file_get_contents('php://input');
+$event = null;
+try {
+  $event = \Stripe\Event::constructFrom(
+    json_decode($payload, true)
+  );
+} catch(\UnexpectedValueException $e) {
+  // Invalid payload
+  echo '⚠️  Webhook error while parsing basic request.';
+  return http_response_code(400);
+  exit();
+}
+// Handle the event
+switch ($event->type) {
+  case 'payment_intent.succeeded':
+    $paymentIntent = $event->data->object; // contains a \Stripe\PaymentIntent
+    // Then define and call a method to handle the successful payment intent.
+    // handlePaymentIntentSucceeded($paymentIntent);
+    break;
+  case 'payment_method.attached':
+    $paymentMethod = $event->data->object; // contains a \Stripe\PaymentMethod
+    // Then define and call a method to handle the successful attachment of a PaymentMethod.
+    // handlePaymentMethodAttached($paymentMethod);
+    break;
+  default:
+    // Unexpected event type
+    echo 'Received unknown event type';
+}
+return http_response_code(200);
+}
 
 
 
