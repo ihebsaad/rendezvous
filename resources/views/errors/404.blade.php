679659d75144749ec3c6468b35c7b97b2dff9088
khoulod <khouloudmabrouk00@gmail.com>
2021-09-02 19:46:25 +0200

    contact

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,46 +9,51 @@
 
 
 <!-- Container -->
-<div class="container NotFound">
+<div id="wrapper" style="background-image: url(http://localhost/rendezvous/public/listeo/images/test.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;">
+<div class="container ">
       
-    <div class="notFound-row">
-    <img src="<?php echo  URL::asset('storage/images/logoprv.png');?>" alt="" class="notFound-img" style="
-    margin-top: 69px;
-">
-    </div>
-	<div class="row"  >
    
-		<div class="col-md-12.notFound" style="margin-left: -35px;">
+	<div class="row"  >
+	<div class="col-lg-6 col-md-12">
+		<br><br><br><br>
+					<img src="<?php echo  URL::asset('storage/images/logoprv.png');?>" alt="" >
 
-			<section id="not-found" >
-				<h3 class="notFound-h3">OUPS! </h3>
-				<center><h4 style="word-spacing: 5px;
-    line-height: 31px;
-    color: black;
-    font-size: 21px;
-    margin-left: 955px;
-    margin-top: 76px;
-    font-family: 'Raleway';;"class="notFound-h4">   Nous sommes désolés, mais la page que vous recherchez n’existe pas. Nous vous invitons de retourner à la page d’accueil. </h4></center>
-
+                        <br><br><br><br><br>
+                  <center> <a href="{{route('home')}}" class="submit button" ><span class="arrow"></span>Revenir à la page d'accueil</a></center> 
+					<br><br></br></div><br><br><br><br><br><br>
+					<div class="col-lg-6 col-md-12" >
+<center>
+					<section id="not-found" >
+						<h3 style="color: #333;
+			font-size: 95px;;
+			font-family: 'Raleway';
+		">OUPS! </h3>
+						<h4 style="hite-space: pre-wrap;
+			word-spacing: 12px;
+			line-height: 37px;
+			color: black;
+			font-size: 21px;
+			font-family: 'Raleway';">	 <strong> Nous sommes désolés, mais la page que vous recherchez n’existe pas. Nous vous invitons de retourner à la page d’accueil. </h4>
+</strong> </center> 
 				<!-- Search -->
-				<div class="row">
-					<div class="col-lg-8 col-lg-offset-2">
-                        <br><br><br>
-                    <a href="{{route('home')}}" class="submit button" ><span class="arrow"></span>Revenir à la page d'accueil</a>
-					</div>
-				</div>
+					
 				<!-- Search Section / End -->
 
 
 			</section>
 
 		</div>
+					<div class="col-lg-2 col-md-2"><br></div>
+	
 	</div>
 
 </div>
 <!-- Container / End -->
 
 
+</div>
 
 <script type="text/javascript" src="{{ asset('public/listeo/scripts/typed.js') }}"></script>
 
