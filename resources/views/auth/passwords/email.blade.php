29069bfdaa0cf4eb64eba8fdc7ee5fe9bfbfa3e8
khoulod <khouloudmabrouk00@gmail.com>
2021-09-03 13:04:44 +0200

    forget_password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,70 @@
-@extends('layouts.app')
+@extends('layouts.frontv2layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
+<div class="wrapper" style="    background: white;
+    display: block;">
+    <div class="container   padding-bottom-100" id="connexionsection">
+    <div class="row margin-top-60">
+        <div class="col-lg-2 col-md-12"></div>
+        <div class="col-lg-8 col-md-12">
+            <div class="dashboard-list-box margin-top-0" style="    margin: 30px 0 0 0;
+    box-shadow: 0 0 12px 0 rgb(0 0 0 / 6%);
+    border-radius: 4px;">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
+                    <form method="POST" action="{{ route('password.email') }}" name="connform" id="connform" autocomplete="on">
                         @csrf
+                        <h4 style="color: #000;
+    background-color: #ffd700;
+    border-radius: 40px 40px 0 0;
+    font-size: 20px;
+    font-weight: 800;
+    text-align: center;
+    padding: 15px 30px;">{{ __('Reset Password') }}</h4>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="form-group row" style="padding: 5px;
+    margin: 0;
+    background-color: #fff;
+}
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
+">
+                            <center>
+                            <div class="row padding-top-60">
+                               
+                                <div class="col-md-12" >
+                                    <div>
+                                    <input id="email" type="email" placeholder="Email Adress" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                    @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
+                                     </div>
+                                </div>
+                                
+                            </div></center>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
+            
+                    <div class="row padding-top-40">
+                                <div class="col-md-12" >
+                                    <button type="submit" class="submit button" id="submitc"  style="    padding: 6px 15px;
+    line-height: 20px;
+    font-size: 13px;
+    font-weight: 600;
+    margin: 0;height:52px   ;width: -webkit-fill-available;"> {{ __('Send Password Reset Link') }}</button>
+                                </div>
                             </div>
-                        </div>
+                      
                     </form>
-                </div>
+                   
             </div>
         </div>
-    </div>
-</div>
+       
+    </div></div>
+
 @endsection
