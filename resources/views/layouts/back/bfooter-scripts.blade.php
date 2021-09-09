aa836c55331e83177eccc7a9650b27783ab2ba63
HaythemTN <haithemsahlia@gmail.com>
2021-09-09 10:56:18 +0100

    modifs email changement page vente

--- resources/views/layouts/back/bfooter-scripts.blade.php
+++ resources/views/layouts/back/bfooter-scripts.blade.php
@@ -1,6 +1,15 @@
 <!-- Scripts
 ================================================== -->
-   
+<!-- Début de widget de badge Calendly -->
+ <?php     $user = auth()->user();
+  $user_type =$user->user_type;
+ ?>
+ <?php if( $user_type=='prestataire'  ){ ?>     
+<link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+<script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+<script type="text/javascript">window.onload = function() { Calendly.initBadgeWidget({ url: 'https://calendly.com/prenezunrendezvous/30min?hide_event_type_details=1&hide_gdpr_banner=1&background_color=020202&text_color=ffffff&primary_color=fff600', text: 'Nous sommes à votre écoute pour toute question du lundi au vendredi de 9h à 17h', color: '#000000', textColor: '#ffffff', branding: false }); }</script>
+<?php } ?>
+<!-- Fin de widget de badge Calendly -->   
 <!-- Date Range Picker - docs: http://www.daterangepicker.com/ -->
  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
