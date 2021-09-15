96f49ad8eff73ccdeff537c05a281467f7f3ab96
khoulod <khouloudmabrouk00@gmail.com>
2021-09-15 17:53:45 +0200

    changes

--- resources/views/users/profile2.blade.php
+++ resources/views/users/profile2.blade.php
@@ -25,13 +25,11 @@
 		<div id="titlebar">
 			<div class="row">
 				<div class="col-md-12">
-					<h2>My Profile</h2>
+					<h2>Profil</h2>
 					<!-- Breadcrumbs -->
 					<nav id="breadcrumbs">
 						<ul>
-							<li><a href="#">Home</a></li>
 							<li><a href="#">Dashboard</a></li>
-							<li>My Profile</li>
 						</ul>
 					</nav>
 				</div>
