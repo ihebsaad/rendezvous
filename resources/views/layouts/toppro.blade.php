dfdcd4696c7e3123deb56904ed43a0de2849f199
khoulod <khouloudmabrouk00@gmail.com>
2021-09-15 13:34:52 +0200

    changes

--- resources/views/layouts/toppro.blade.php
+++ resources/views/layouts/toppro.blade.php
@@ -199,7 +199,7 @@ $plogo= $parametres->logo;
             <div id="sign-in-dialog2" class="zoom-anim-dialog mfp-hide">
 
                 <div class="small-dialog-header">
-                    <h3  style="text-align:center;">Inscrivez-vous en 2 minutes seulement</h3>
+                    <h3  style="text-align:center;color:gray;">Inscrivez-vous en 2 minutes seulement</h3>
                 </div>
 
 
