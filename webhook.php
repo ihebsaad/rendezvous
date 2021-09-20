d9df22cf95acde51febdb0b4b120ca072e87dc04
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-09-20 14:15:18 +0200

    test

--- webhook.php
+++ webhook.php
@@ -14,11 +14,11 @@
 require 'vendor/autoload.php';
 \Stripe\Stripe::setApiKey('sk_test_51IyZEOLYsTAPmLSFOUPFtTTEusJc2G7LSMDZEYDxBsv0iJblsOpt1dfaYu8PrEE6iX6IX7rCbpifzhdPfW7S0lzA007Y8kjGAx');
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
-$endpoint_secret = 'whsec_rwPn2MsYhTNgrGqNu6jnbVPFvQlzxnQY';
+$endpoint_secret = 'whsec_xKKyAxoEUKpozExCw8I0anJj9PpBKCcD';
 
 $payload = @file_get_contents('php://input');
 $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
-dd($sig_header);
+//dd($sig_header);
 $event = null;
 
 try {
