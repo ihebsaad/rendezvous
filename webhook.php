00b352bbafa80a808cf536332c22b8117f99b6c5
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-09-23 16:51:15 +0200

    test

--- webhook.php
+++ webhook.php
@@ -85,7 +85,7 @@ elseif ($event->type=='customer.subscription.updated') {
   # code...
 }
 elseif ($event->type=='invoice.payment_failed') {
-  echo ($event->type);
+  echo ($event->data->object->lines->data->subscription);
   if ($event->lines->data->subscription==null) {
 echo "null";
     http_response_code(200);
