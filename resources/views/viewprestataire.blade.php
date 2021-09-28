cae1aec0a480e15101d4d0aedc02f07283bc6c1b
khoulod <khouloudmabrouk00@gmail.com>
2021-09-28 15:15:09 +0200

    changes

--- resources/views/viewprestataire.blade.php
+++ resources/views/viewprestataire.blade.php
@@ -660,11 +660,10 @@ table.basic-table th {
                 
                 
                 <!-- Listing Contacts -->
-                <div class="listing-links-container">
+                <div class="">
 
-                    <ul class="listing-links contact-links">
-                        <li><a href="tel:{{$user->tel}}" class="listing-links"><i class="fa fa-phone"></i> {{$user->tel}}</a></li>
-                        <li><a style="display: inline-block!important;width:40px;"href="mailto:{{$user->email}}" class="listing-links"><i class="fa fa-envelope-o"></i> {{$user->email}}</a>
+                    <ul class="">
+                        <li><a href="tel:{{$user->tel}}" class="listing-links"><i class="fa fa-phone"></i> {{$user->email}}</a></li>
                         </li>
                         <!--<li><a href="#" target="_blank"  class="listing-links"><i class="fa fa-link"></i> www.example.com</a></li>-->
                     </ul>
