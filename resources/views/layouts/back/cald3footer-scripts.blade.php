0083613a1e4b7f13072e5b7e92342dfe023b159c
khoulod <khouloudmabrouk00@gmail.com>
2021-10-01 10:43:04 +0200

    changes

--- resources/views/layouts/back/cald3footer-scripts.blade.php
+++ resources/views/layouts/back/cald3footer-scripts.blade.php
@@ -37,6 +37,18 @@
 
 
 <script>
+  $(".time-slot").each(function() {
+
+var timeSlot = $(this);
+$(this).find('input').on('change',function() {
+    
+    var timeSlotVal = timeSlot.find('strong').text();
+
+    $('.panel-dropdown.time-slots-dropdown a').html(timeSlotVal);
+    $('.panel-dropdown').removeClass('active');
+});
+});
+ 
 $(function() {
   moment.lang('fr');
     var start = moment().subtract(29, 'days');
