0a2869685a4ebbaaa21f0a32943f10a12def8798
khoulod <khouloudmabrouk00@gmail.com>
2021-10-05 16:54:30 +0200

    changes

--- resources/views/layouts/calend3layout.blade.php
+++ resources/views/layouts/calend3layout.blade.php
@@ -27,6 +27,8 @@
   <link href="{{URL::asset('public/fullcalendar3/css/bootstrap-colorpicker.css')}}" rel="stylesheet">
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <meta name="viewport" content="width=1000">
+
 </head>
 <body>
 <!-- Wrapper -->
