cc8c5530138504b9081c83f896935267dcb794ef
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-10-05 13:45:59 +0200

    test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -84,7 +84,7 @@ class LoginController extends Controller
             {
               if ($user->invoiceStripe == 0 ) {
                 //dd("okfffffff");
-                Auth::logout();
+                logout();
                 return redirect ('/Facture_Impayee');
                 
               }else{
