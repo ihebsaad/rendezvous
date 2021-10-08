0e584651f77b428117adbe8f5618312a46c7cd84
MedAchrafBesbes <mohamed.achraf.besbes@gmail.com>
2021-10-08 15:19:59 +0200

    correct

--- resources/views/payments/pay.blade.php
+++ resources/views/payments/pay.blade.php
@@ -78,7 +78,8 @@
            <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
   //alert("{{ $Res }}");
-    var stripe = Stripe('pk_live_51Hbt14Go3M3y9uW5wKqzuQN968NPlPtwzB5UZFzLqTtNAZxoU43qINNnPR7cAO8j1XKoZZ1QZyGkYLHvQjOc5U3c00K0ZoDEMa', {
+  //pk_live_51Hbt14Go3M3y9uW5wKqzuQN968NPlPtwzB5UZFzLqTtNAZxoU43qINNnPR7cAO8j1XKoZZ1QZyGkYLHvQjOc5U3c00K0ZoDEMa
+    var stripe = Stripe('pk_test_51IyZEOLYsTAPmLSFNL9DwqmtcBONlT5sTZFcGE3NXBLvYOxVG0L8XicQaTq4KxFYmOJX42jAqCw7QJ1qOFFWjfwp00xPjV3V4L', {
   stripeAccount: "{{ $idaccount }}"
 });
 
