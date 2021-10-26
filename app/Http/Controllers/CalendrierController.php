1ec15e6744391215bbb03fde321fd5d4546b411b
kbskhaled <kbskhaled@gmail.com>
2021-10-26 11:48:00 +0100

    correct

--- app/Http/Controllers/CalendrierController.php
+++ app/Http/Controllers/CalendrierController.php
@@ -1499,7 +1499,7 @@ class CalendrierController extends Controller
    //dd(self::$tab_minutes_indisp_rendezvous);
    // calculate the start and the end of simple service réservation
 
-   $idservicessimples=Service::where('recurrent','like','off')->where("NbrService",1)->pluck('id')->toArray();
+   $idservicessimples=Service::where('recurrent','like','off')->where("NbrService",'>=',1)->pluck('id')->toArray();
    for($i=0;$i<count($idservicessimples);$i++)
      {
        $idservicessimples[$i]=strval($idservicessimples[$i]);
